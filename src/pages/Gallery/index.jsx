c98f66badf06faec923687ea161040f4829c2614
firmangel8 <firmangel8@gmail.com>
2025-04-28 19:40:23 +0800

    add toast

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -1,11 +1,39 @@
-import { Col, Row, Typography, Card, List, Skeleton, Divider } from "antd";
+import {
+  Col,
+  Row,
+  Typography,
+  Card,
+  List,
+  Skeleton,
+  Divider,
+  FloatButton,
+  Drawer,
+  Form,
+  Input,
+  Button,
+  Select,
+  notification,
+} from "antd";
 const { Title, Text } = Typography;
-import { getData } from "../../utils/api";
+const { Option } = Select;
+import { getData, sendData } from "../../utils/api";
 import { useState, useEffect } from "react";
+import { CustomerServiceOutlined } from "@ant-design/icons";
 
 const Gallery = () => {
+  const [api, contextHolder] = notification.useNotification();
   const [dataSources, setDataSources] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const [form] = Form.useForm();
+
+  const openNotificationWithIcon = (type, msg) => {
+    api[type]({
+      message: "Notification Title",
+      description: msg,
+    });
+  };
 
   useEffect(() => {
     getDataGallery();
@@ -26,8 +54,84 @@ const Gallery = () => {
       });
   };
 
+  const onCloseDrawer = () => {
+    setIsOpenDrawer(false);
+  };
+
+  const handleDrawer = () => {
+    setIsOpenDrawer(true);
+  };
+
+  const handleSubmit = () => {
+    // eksekusi disini
+    let nameOfNatures = form.getFieldValue("name_of_natures");
+    let descriptionOfNatures = form.getFieldValue("description_of_natures");
+
+    let endpoint = "/api/v1/natures";
+    let formData = new FormData();
+    formData.append("name_natures", nameOfNatures);
+    formData.append("description", descriptionOfNatures);
+
+    sendData(endpoint, formData)
+      .then((resp) => {
+        if (resp?.datas) {
+          setIsOpenDrawer(false);
+          openNotificationWithIcon("success", "Berhasil menambahkan data");
+          getDataGallery();
+        } else {
+          openNotificationWithIcon("error", "Gagal Menambahkan data");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const renderDrawer = () => {
+    return (
+      <Drawer
+        width={"550px"}
+        title="Form Input Natures"
+        onClose={onCloseDrawer}
+        open={isOpenDrawer}
+        extra={
+          <>
+            <Button type="primary" onClick={() => handleSubmit()}>
+              Submit
+            </Button>
+          </>
+        }
+      >
+        <Form form={form} layout="vertical">
+          <Form.Item
+            label="Name of natures"
+            name="name_of_natures"
+            rules={[{ required: true, message: "Name of natures must filled" }]}
+          >
+            <Input placeholder="eg. Mountain" />
+          </Form.Item>
+          <Form.Item
+            label="Description of natures"
+            name="description_of_natures"
+          >
+            <Input.TextArea placeholder="eg. Mountain" rows={3} />
+          </Form.Item>
+        </Form>
+      </Drawer>
+    );
+  };
+
   return (
     <div className="layout-content">
+      {contextHolder}
+      <FloatButton
+        shape="circle"
+        type="primary"
+        icon={<CustomerServiceOutlined />}
+        onClick={() => handleDrawer()}
+      />
+      {renderDrawer()}
+
       <Row gutter={[24, 0]}>
         <Col xs={23} className="mb-24">
           <Card bordered={false} className="circlebox h-full w-full">
